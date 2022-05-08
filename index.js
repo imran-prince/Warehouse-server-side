eae1eb433a8bb0958eb77330ad6b3f8570b0d283
imran-hossain <csebubt35@gmail.com>
2022-05-08 14:56:24 +0600

    env variavble

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json())
 
 // dbpass:5WT1w6kjyfBySCMX
 // dbname:imran
-const uri = `mongodb+srv://imran:5WT1w6kjyfBySCMX@cluster0.3s80f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${DB_User}:${DB_PASS}@cluster0.3s80f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
   try {
@@ -28,6 +28,22 @@ async function run() {
        res.send(result)
        console.log(result)
     })
+    app.put('/product/:id', async (req, res) => {
+        const id = req.params.id
+        const updateProduct = req.body
+        console.log(updateProduct.quantity);
+        const filter = { _id: ObjectId(id) }
+        const option = { upsert: true }
+        const updatedoc = {
+          $set: {
+               quantity: updateProduct.quantity
+          }
+        }
+        const result = await userColloectin.updateOne(filter, updatedoc, option)
+        res.send(updateProduct.quantity)
+        console.log(updateProduct.quantity);
+      
+  })
     // Delet User
     // app.delete('/product/:id', async (req, res) => {
     //   const id = req.params.id
@@ -35,13 +51,13 @@ async function run() {
     //   const result = await userColloectin.deleteOne(query)
     //   res.send(result)
     // })
-    // // // update paticular
-    // // app.get('/user/:id', async (req, res) => {
-    // //   const id = req.params.id
-    // //   const query = { _id: ObjectId(id) }
-    // //   const result = await userColloectin.findOne(query)
-    // //   res.send(result)
-    // // })
+    // update paticular
+    app.get('/product/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: ObjectId(id) }
+      const result = await productCollection.findOne(query)
+      res.send(result)
+    })
     // // put method for updata
     // app.put('/product/:id', async (req, res) => {
     //   const id = req.params.id

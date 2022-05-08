0c2b9b4cea196200b561a01924d4408b8790f979
imran-hossain <csebubt35@gmail.com>
2022-05-08 11:31:13 +0600

    post api add

--- index.js
+++ index.js
@@ -2,48 +2,69 @@ const express = require('express');
 const app = express()
 const cors = require('cors')
 const { MongoClient, ServerApiVersion } = require('mongodb')
-const ObjetId = require('mongodb').ObjectId
+const ObjectId = require('mongodb').ObjectId
 const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
 // dbpass:5WT1w6kjyfBySCMX
 // dbname:imran
-
-// const { MongoClient, ServerApiVersion } = require('mongodb');
- 
- 
- 
-
-
-
-
- 
 const uri = `mongodb+srv://imran:5WT1w6kjyfBySCMX@cluster0.3s80f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-  
- 
-// });
-async function run(){
-  try{
+async function run() {
+  try {
     await client.connect()
-    const collection = client.db("imran").collection("computer");
-  
-    app.get('/allproduct',async(req,res)=>{
-      const q=req.query
-      const cursor=collection.find({})
-      const result=await cursor.toArray()
+    const productCollection = client.db("imran").collection("computer");
+
+    app.get('/allproduct', async (req, res) => {
+      const q = req.query
+      const cursor = productCollection.find({})
+      const result = await cursor.toArray()
       res.send(result)
     })
-   
+    app.post('/productadd', async(req,res)=>{
+       const newProduct=req.body;
+       const result=await productCollection.insertOne(newProduct)
+       res.send(result)
+       console.log(result)
+    })
+    // Delet User
+    // app.delete('/product/:id', async (req, res) => {
+    //   const id = req.params.id
+    //   const query = { _id: ObjectId(id) }
+    //   const result = await userColloectin.deleteOne(query)
+    //   res.send(result)
+    // })
+    // // // update paticular
+    // // app.get('/user/:id', async (req, res) => {
+    // //   const id = req.params.id
+    // //   const query = { _id: ObjectId(id) }
+    // //   const result = await userColloectin.findOne(query)
+    // //   res.send(result)
+    // // })
+    // // put method for updata
+    // app.put('/product/:id', async (req, res) => {
+    //   const id = req.params.id
+    //   const updateUser = req.body
+    //   const filter = { _id: ObjectId(id) }
+    //   const option = { upsert: true }
+    //   const updatedoc = {
+    //     $set: {
+    //       name: updateUser.name,
+    //       email: updateUser.email
+
+    //     }
+    //   }
+    //   const result = await userColloectin.updateOne(filter, updatedoc, option)
+    //   res.send(result)
+
+    // })
 
 
 
 
   }
-  finally{
+  finally {
 
   }
 }
@@ -51,8 +72,8 @@ run().catch(console.dir)
 
 
 app.get('/', (req, res) => {
-    res.send('computer accessories server test ok')
+  res.send('computer accessories server test ok')
 })
 app.listen(port, () => {
-    console.log("my port", port);
+  console.log("my port", port);
 })
